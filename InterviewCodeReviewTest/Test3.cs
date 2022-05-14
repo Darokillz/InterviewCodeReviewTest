e8ef15aef8590bd50d8041b75bf2d84dc2243bd0
Darren Lai <100732475+Darokillz@users.noreply.github.com>
2022-05-14 17:09:40 +1200

    added comments

--- InterviewCodeReviewTest/Test3.cs
+++ InterviewCodeReviewTest/Test3.cs
@@ -16,6 +16,9 @@ namespace InterviewCodeReviewTest
 			// Assign each email to different thread for performance
 			public void SendNextEmail()
 			{
+				// - Consider using threadpool due to a posibility of high volumne of requests
+				//  problem may arise such as context swtiching, thread creation and large overhead
+				//  use ThreadPool.QueueUserWorkItem(SendEmail);
 				var thread = new Thread(SendEmail);
 				thread.Start();
 			}
@@ -29,6 +32,10 @@ namespace InterviewCodeReviewTest
 
 			private void UpdateStatistics(Result result)
 			{
+				// - lock(typeof(EmailSendQueue) is a bad way of locking.
+				// This is due to the object EmailSendQueue is public thus widely accesible with the possibility of introducing deadlocks.
+				// A solution will be creating a private object in the emailSendQueue class ie. Private object _updateLock = new object();
+				// and change lock(typeof(EmailSendQueue) into lock(_updateLock). 
 				lock (typeof(EmailSendQueue))
 				{
 					if (result.IsSuccessful)
